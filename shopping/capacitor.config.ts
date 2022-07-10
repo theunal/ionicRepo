170d92dac478c6bbdfc43fcf87b96440fc7e97e3
Yusuf Ünal <97359852+theunal@users.noreply.github.com>
2022-07-10 16:30:16 +0300

    created shoppinh app files

---
+++ shopping/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'shopping',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
