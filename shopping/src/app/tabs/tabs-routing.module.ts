1c89da8864b7f0f21ed0d9877b13b4c13fc16e17
Yusuf Ünal <97359852+theunal@users.noreply.github.com>
2022-07-17 22:44:33 +0300

    Update tabs-routing.module.ts
    routing

--- shopping/src/app/tabs/tabs-routing.module.ts
+++ shopping/src/app/tabs/tabs-routing.module.ts
@@ -29,7 +29,8 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
-  }
+  },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
